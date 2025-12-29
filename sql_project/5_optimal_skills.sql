3c0368d2e7bfc06b49d82ecdd767aa314e36eee1
ali hassan <ali.hass927@gmail.com>
2025-12-29 00:25:00 -0800

    cvs

--- sql_project/5_optimal_skills.sql
+++ sql_project/5_optimal_skills.sql
@@ -10,7 +10,7 @@ WITH high_demand_skills AS (
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE
-        job_postings_fact.job_title_short = 'Business Analyst' 
+        job_postings_fact.job_title_short = 'Data Analyst' 
         AND job_postings_fact.job_country = 'United States'
         AND job_postings_fact.salary_year_avg IS NOT NULL
     GROUP BY
@@ -28,7 +28,7 @@ high_salary_skills AS (
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
     WHERE
-        job_postings_fact.job_title_short = 'Business Analyst' 
+        job_postings_fact.job_title_short = 'Data Analyst' 
         AND job_postings_fact.job_country = 'United States'
         AND job_postings_fact.salary_year_avg IS NOT NULL
     GROUP BY
